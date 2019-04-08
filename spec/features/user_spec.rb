7f5062e2ddbaa610b98bae96b262b97e01486a8d
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-04-08 18:03:27 +0000

    Add email and password to user views

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,8 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Contact Management', js: true do
+RSpec.feature 'User Management', js: true do
   let!(:user) { FactoryBot.create(:user) }
+  let!(:user_info) { FactoryBot.build(:user) }
 
   scenario 'should redirect to login if I am not logged in' do
     visit root_path
@@ -24,5 +25,26 @@ RSpec.feature 'Contact Management', js: true do
     click_link user.full_name
     click_link 'Logout'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+
+  scenario 'should edit a user' do
+    login_as user
+
+    visit edit_user_path user
+    fill_in :user_first_name, with: user_info.first_name
+    fill_in :user_last_name, with: user_info.last_name
+    fill_in :user_email, with: user_info.email
+    fill_in :user_password, with: user_info.password
+    click_button 'Update User'
+    expect(page).to have_content 'User was successfully updated.'
+  end
+
+  scenario 'should view the details of a user ' do
+    login_as user
+
+    visit user_path user
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.last_name
+    expect(page).to have_content user.email
   end
 end
