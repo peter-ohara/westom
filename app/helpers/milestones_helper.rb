e3bc97a29630f88185771f1e01738fed214bea74
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-04-09 08:53:57 +0000

    Add feature: I should be able to set targets for a department

--- app/helpers/milestones_helper.rb
+++ app/helpers/milestones_helper.rb
@@ -1,2 +1,19 @@
 module MilestonesHelper
+  def milestone_percentage(milestone)
+    return "#{(milestone.progress.to_f / milestone.target) * 100}%" if milestone.target_type == :deals.to_s
+
+    "#{(milestone.progress / milestone.target) * 100}%"
+  end
+
+  def milestone_fraction(milestone)
+    return "#{milestone.progress} / #{milestone.target}" if milestone.target_type == :deals.to_s
+
+    "#{humanized_money_with_symbol milestone.progress} / #{humanized_money_with_symbol milestone.target}"
+  end
+
+  def format_target(milestone)
+    return milestone.target if milestone.target_type == :deals.to_s
+
+    humanized_money_with_symbol milestone.target
+  end
 end
--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -9,7 +9,25 @@ class Milestone < ApplicationRecord
   enum status: { active: 0,
                  archived: 1 }
 
+  def deals
+    Deal.closed.where('date_of_closing > ? AND date_of_closing < ?', start_date, due_date)
+  end
+
   def progress
-    100
+    return deals.count if target_type == :deals.to_s
+
+    deals.sum(:amount_cents).to_money / 100
+  end
+
+  def target
+    return self[:target].to_i if target_type == :deals.to_s
+
+    self[:target].to_money
+  end
+
+  def target_name
+    return 'Deals' if target_type == :deals.to_s
+
+    'Revenue'
   end
 end
